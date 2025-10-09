2e50c84521e7dfb6fa85a1d667f1f41bb846a607
Sean <sean@albinodrought.com>
2025-10-08 23:37:12 -0700

    fix: better mobile view for LiveView page

--- ui/src/views/LiveView.vue
+++ ui/src/views/LiveView.vue
@@ -30,11 +30,11 @@ const streamStore = useStreamStore();
       </button>
     </div> -->
     
-    <div class="flex-1 grid grid-cols-3 gap-2 p-2 bg-gray-900" style="grid-template-rows: repeat(auto-fill, 32.5vh)">
-      <div 
-        v-for="(stream, i) in streamStore.streams" 
-        :key="stream.id" 
-        :class="['bg-gray-700 flex items-center justify-center', { 'col-span-2 row-span-2': (i % 6) == 0 }]"
+    <div class="flex-1 grid grid-cols-1 md:grid-cols-3 gap-2 p-2 bg-gray-900 auto-rows-[minmax(250px,1fr)] md:auto-rows-[auto]" style="grid-template-rows: repeat(auto-fill, 32.5vh)">
+      <div
+        v-for="(stream, i) in streamStore.streams"
+        :key="stream.id"
+        :class="['bg-gray-700 flex items-center justify-center', { 'md:col-span-2 md:row-span-2': (i % 6) == 0 }]"
       >
         <VideoStream
           class="w-full h-full"
